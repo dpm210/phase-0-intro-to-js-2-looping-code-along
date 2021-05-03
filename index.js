17f1291f143fa3113150b66308629ed2fe9f66cb
Daniel Mueller <danielpetermueller@gmail.com>
2021-05-03 13:49:23 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const names = ["Lisa", "Kaitlin", "Jan"];
+function writeCards(names) {
+    let newNames = [];
+    for (let i = 0; i < names.length; i++)[
+    newNames.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    ]
+    return newNames;
+}
+function countDown(number){
+     while (number >= 0) {
+        console.log(number);
+        number--;
+      } 
+}
+countDown(4);
